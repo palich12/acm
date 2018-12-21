33492ea030448b697c1bc5fc8061cbe5ebaedcee
palich12 <i@agibalov.ru>
2018-12-21 16:47:00 +0300

    c# hotfix

--- TulaI/TulaI/Program.cs
+++ TulaI/TulaI/Program.cs
@@ -5,14 +5,15 @@ using System.Collections.Generic;
 namespace TulaI
 {
 
-    public class Entity : IComparable
+    public class Section : IComparable
     {
-        public List<int> path;
-        public int w;
+        public int I;
+        public int J;
+        public int Value;
 
         public int CompareTo(object obj)
         {
-            return w.CompareTo(((Entity)obj).w);
+            return Value.CompareTo(((Section)obj).Value);
         }
     }
 
@@ -28,6 +29,84 @@ namespace TulaI
         public static Dictionary<Int64, int> cash = new Dictionary<Int64, int>();
         public static int BestScore = Int32.MaxValue;
         public static int Counter = 0;
+
+        static int req( List<int> path, int lastSectionValue )
+        {
+            if (path.Count <= 3)
+            {
+                return 0;
+            }
+
+            var pathhash = GetHash(path);
+            if ( cash.ContainsKey(pathhash)) 
+            {
+                return cash[pathhash];
+            }
+
+            var sections = new List<Section>();
+            for (int i = 0; i < path.Count - 2; i++)
+            {
+
+                for (int j = i+2; j < path.Count && !(i==0 && j == path.Count-1); j++)
+                {
+                    var v = Matrix[path[i], path[j]];
+                    if( v < lastSectionValue)
+                    {
+                        continue;
+                    }
+                    sections.Add(new Section()
+                    {
+                        I = i,
+                        J = j,
+                        Value = v
+                    });
+
+                    
+
+                }            
+            }
+
+            sections.Sort();
+            var res = Int32.MaxValue;
+            foreach (var section in sections)
+            {
+                var newpath1 = new List<int>();
+                var newpath2 = new List<int>();
+                int k = 0;
+                foreach (var vert in path)
+                {
+                    if (k <= section.I || k >= section.J)
+                    {
+                        newpath1.Add(vert);
+                    }
+                    if (k >= section.I && k <= section.J)
+                    {
+                        newpath2.Add(vert);
+                    }
+
+                    k++;
+                }
+
+                var r1 = req(newpath1, section.Value);
+                var r2 = req(newpath2, section.Value);
+                res = Math.Min(res, r1 == int.MaxValue || r2 == int.MaxValue ? int.MaxValue : r1 + r2 + section.Value);
+            }
+
+            cash[pathhash] = res;
+            return res;
+        }
+
+        public static Int64 GetHash(List<int> path)
+        {
+            Int64 res = 1;
+            for ( var i = 0; i < path.Count; i++)
+            {
+                //res *= i;
+                res *= 500;
+                res += path[i];
+            }
+            return res;  
+        }
 
         static void Main(string[] args)
         {
@@ -49,70 +128,10 @@ namespace TulaI
             {
                 path.Add(i);
             }
-            Console.WriteLine(req(path));
+            Console.WriteLine(req(path,0));
             Console.ReadLine();
             
 
-        }
-
-        static int req( List<int> path )
-        {
-            if (path.Count <= 3)
-            {
-                return 0;
-            }
-
-            var pathhash = GetHash(path);
-            if ( cash.ContainsKey(pathhash)) 
-            {
-                return cash[pathhash];
-            }
-
-            var res = Int32.MaxValue;
-            
-            for (int i = 0; i < path.Count - 2; i++)
-            {
-
-                for (int j = i+2; j < path.Count && !(i==0 && j == path.Count-1); j++)
-                {
-                    var newpath1 = new List<int>();
-                    var newpath2 = new List<int>();
-                    int k = 0;
-                    foreach(var vert in path)
-                    {
-                        if(k <= i || k >= j)
-                        {
-                            newpath1.Add(vert);
-                        }
-                        if( k >= i && k <= j)
-                        {
-                            newpath2.Add(vert);
-                        }
-
-                        k++;
-                    }
-
-                    var r1 = req(newpath1);
-                    var r2 = req(newpath2);
-                    res = Math.Min(res, r1 == int.MaxValue || r2 == int.MaxValue ? int.MaxValue : r1 + r2 + Matrix[path[i], path[j]]);
-
-                }            
-            }
-
-            cash[pathhash] = res;
-            return res;
-        }
-
-        public static Int64 GetHash(List<int> path)
-        {
-            Int64 res = 1;
-            for ( var i = 0; i < path.Count; i++)
-            {
-                //res *= i;
-                res *= 500;
-                res += path[i];
-            }
-            return res;  
         }
     }
 }
