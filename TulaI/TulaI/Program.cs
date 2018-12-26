d858d85f39c2e8ec6191bf4b9a6dd51fa38bace3
palich12 <i@agibalov.ru>
2018-12-26 22:12:11 +0300

    Breadth-first search

--- TulaI/TulaI/Program.cs
+++ TulaI/TulaI/Program.cs
@@ -4,35 +4,42 @@ using System.Collections.Generic;
 
 namespace TulaI
 {
-
-    public class Section : IComparable
+    class Task : IComparable
     {
-        public int I;
-        public int J;
+        public static int Indexer = 0;
+        public int Index;
         public int Value;
+        public int[] Path;
+
+        public Task()
+        {
+            Index = Indexer;
+            Indexer++;
+        }
 
         public int CompareTo(object obj)
         {
-            return Value.CompareTo(((Section)obj).Value);
+            var r = Value.CompareTo((obj as Task).Value);
+            if(r == 0)
+            {
+                return Index.CompareTo((obj as Task).Index);
+            }
+            return r;
         }
     }
 
-    public class ReqResult
-    {
-        public int Result;
-        public bool Uncash;
-    }
     class Program
     {
         public static int[,] Matrix;
         public static int N;
         public static Dictionary<Int64, int> cash = new Dictionary<Int64, int>();
+        public static SortedSet<Task> Pipe = new SortedSet<Task>();
         public static int BestScore = Int32.MaxValue;
         public static int Counter = 0;
 
-        static int req( List<int> path, int lastSectionValue )
+        static int Req( int[] path)
         {
-            if (path.Count <= 3)
+            if (path.Length <= 3)
             {
                 return 0;
             }
@@ -41,65 +48,24 @@ namespace TulaI
             if ( cash.ContainsKey(pathhash)) 
             {
                 return cash[pathhash];
-            }
-
-            var sections = new List<Section>();
-            for (int i = 0; i < path.Count - 2; i++)
-            {
-
-                for (int j = i+2; j < path.Count && !(i==0 && j == path.Count-1); j++)
-                {
-                    var v = Matrix[path[i], path[j]];
-                    if( v < lastSectionValue)
-                    {
-                        continue;
-                    }
-                    sections.Add(new Section()
-                    {
-                        I = i,
-                        J = j,
-                        Value = v
-                    });
-
-                    
-
-                }            
-            }
-
-            sections.Sort();
+            }  
             var res = Int32.MaxValue;
-            foreach (var section in sections)
+            for ( int i = 0; i < path.Length; i ++)
             {
-                var newpath1 = new List<int>();
-                var newpath2 = new List<int>();
-                int k = 0;
-                foreach (var vert in path)
-                {
-                    if (k <= section.I || k >= section.J)
-                    {
-                        newpath1.Add(vert);
-                    }
-                    if (k >= section.I && k <= section.J)
-                    {
-                        newpath2.Add(vert);
-                    }
-
-                    k++;
-                }
-
-                var r1 = req(newpath1, section.Value);
-                var r2 = req(newpath2, section.Value);
-                res = Math.Min(res, r1 == int.MaxValue || r2 == int.MaxValue ? int.MaxValue : r1 + r2 + section.Value);
+                var newpath = new int[path.Length - 1];
+                Array.Copy(path, 0, newpath, 0, i);
+                Array.Copy(path, i + 1, newpath, i, path.Length - i - 1);
+                var r = Req(newpath);
+                res = Math.Min(res, r == int.MaxValue ? int.MaxValue : r + Matrix[path[i == 0 ? path.Length - 1 : i - 1], path[i == path.Length - 1 ? 0 : i + 1]]);
             }
-
             cash[pathhash] = res;
             return res;
         }
 
-        public static Int64 GetHash(List<int> path)
+        public static Int64 GetHash(int[] path)
         {
             Int64 res = 1;
-            for ( var i = 0; i < path.Count; i++)
+            for ( var i = 0; i < path.Length; i++)
             {
                 //res *= i;
                 res *= 500;
@@ -123,12 +89,44 @@ namespace TulaI
             }
 
             //req(new List<int>());
-            var path = new List<int>();
+            var path = new int[N];
             for(int i = 0; i < N; i++)
             {
-                path.Add(i);
+                path[i] = i;
             }
-            Console.WriteLine(req(path,0));
+
+            Pipe.Add( new Task
+            {
+                Path = path,
+                Value = 0
+            });
+
+            while(Pipe.Count > 0)
+            {
+                Task task = (Task)Pipe.Min;
+                Pipe.Remove(task);
+
+                if ( task.Path.Length <= 3)
+                {
+                    BestScore = task.Value;
+                    break;
+                }
+                for (int i = 0; i < task.Path.Length; i++)
+                {
+                    var newpath = new int[task.Path.Length - 1];
+                    Array.Copy(task.Path, 0, newpath, 0, i);
+                    Array.Copy(task.Path, i + 1, newpath, i, task.Path.Length - i - 1);
+                    var value = task.Value + Matrix[task.Path[i == 0 ? task.Path.Length - 1 : i - 1], task.Path[i == task.Path.Length - 1 ? 0 : i + 1]];
+                    Pipe.Add( new Task()
+                    {
+                        Value = value,
+                        Path = newpath
+                    });                                    
+                }
+
+            }
+
+            Console.WriteLine(BestScore);
             Console.ReadLine();
             
 
