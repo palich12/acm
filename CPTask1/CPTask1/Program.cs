d86889ecf18a67542c0d2961a82f8cf86b48493c
palich12 <i@agibalov.ru>
2019-02-17 19:31:34 +0300

    for programmers club

---
+++ CPTask1/CPTask1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace CPTask1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int res = 0;
+            for(int i = 0; i < 10000; i++)
+            {
+                int a = i / 1000;
+                int b = (i / 100) % 10;
+                int c = (i / 10) % 10;
+                int d = i % 10;
+
+                if(    Math.Abs(a - b) > 1
+                    && Math.Abs(b - c) > 1
+                    && Math.Abs(c - d) > 1)
+                {
+                    res++;
+                }
+            }
+            Console.WriteLine(res.ToString());
+            Console.ReadLine();
+        }
+    }
+}
